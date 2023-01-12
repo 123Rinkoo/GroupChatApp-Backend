85e58184514d550dc17cfa4b07a375a6085b589c
rinkoo <rinkookandpal1966@gmail.com>
2023-01-12 15:02:31 +0530

    optimising the chat app

--- controller/message.js
+++ controller/message.js
@@ -1,7 +1,8 @@
 const Message=require('../model/messages');
-
+const { Op } = require("sequelize");
 exports.sendmessage=(req,res,next)=>{
 //  console.log(req.body.key1, 'this is userid',req.user.id);
+
  Message.create({Message: req.body.key1, userId: req.user.id, UserName: req.user.name})
  .then(result=> {
     return res.status(200).json({success: true})})
@@ -10,7 +11,16 @@ exports.sendmessage=(req,res,next)=>{
 
 exports.getmessage=(req, res, next)=>{
     // console.log('its coming');
-    Message.findAll()
+    if(req.query.lastId==undefined)
+    {
+       var  lastmessageId=-1;
+    }
+    else
+    {
+        var lastmessageId=req.query.lastId;
+    }
+    console.log(req.query.lastId);
+    Message.findAll({where: {id: {[Op.gt]:lastmessageId}}})
     .then(messages=>res.json(messages))
     .catch(err=>console.log(err));
 }
--- public/chat.js
+++ public/chat.js
@@ -1,31 +1,50 @@
 window.onload = function () {
-    axios.get('http://localhost:4000/getmessage')
-        .then(messages => ShowingMessage(messages.data))
+    axios.get('http://localhost:4000/getmessage ')
+        .then(messages => {
+            localStorage.setItem('msgArray', JSON.stringify(messages.data))
+            ShowingMessage();
+            // console.log('this is last id: ', messages)
+        })
         .catch(err => console.log(err));
 }
 
 setInterval(() => {
-    axios.get('http://localhost:4000/getmessage')
-        .then(messages => ShowingMessage(messages.data))
+    const messages = JSON.parse(localStorage.getItem('msgArray'));
+    const lastmessageId = messages[messages.length - 1].id;
+    // console.log('this is last id: ',lastmessageId)
+    axios.get(`http://localhost:4000/getmessage?lastId=${lastmessageId}`)
+        .then(messages => {
+            const OldMessages = JSON.parse(localStorage.getItem('msgArray'));
+            const MergedMessages = OldMessages.concat(messages.data);
+            localStorage.setItem('msgArray', JSON.stringify(MergedMessages))
+            ShowingMessage();
+        })
         .catch(err => console.log(err));
 }, 1000);
 
-// setInterval(() => {
-//     window.location.href = 'chat.html'
-// }, 1000);
+function ShowingMessage() {
+    const messages = JSON.parse(localStorage.getItem('msgArray'));
+    if(messages.length>20){
+        var a=messages.length-20;
+        // console.log(a, messages.length);
+        const removeFirstTwenty = messages.splice(a);
+        console.log(removeFirstTwenty);
 
-function ShowingMessage(messages) {
+        localStorage.setItem('msgArray', JSON.stringify(removeFirstTwenty));
+        
+    }
+    const limitedmessage=JSON.parse(localStorage.getItem('msgArray'));
+
     id1 = document.getElementById('chatmessages');
-    id1.innerHTML="";
-    
-  for(i=0; i<messages.length; i++)
-  {
-      id2 = `<div class="cm">
-      <p>${messages[i].UserName}: ${messages[i].Message}</p>
+    id1.innerHTML = "";
+
+    for (i = 0; i < limitedmessage.length; i++) {
+        id2 = `<div class="cm">
+      <p>${limitedmessage[i].UserName}: ${limitedmessage[i].Message}</p>
     </div>
     <hr>`
-    id1.innerHTML=id1.innerHTML+id2;
-  }
+        id1.innerHTML = id1.innerHTML + id2;
+    }
 }
 
 function messages(event) {
@@ -33,9 +52,10 @@ function messages(event) {
     const token = localStorage.getItem('token');
     console.log(token);
     const message = event.target.message.value;
+    event.target.message.value="";
     axios.post('http://localhost:4000/sendmessage', { key1: message }, { headers: { 'Authorization': token } })
         .then(result => {
-            window.location.href = 'chat.html'
+            // window.location.href = 'chat.html'
         })
         .catch(err => console.log(err));
 }
